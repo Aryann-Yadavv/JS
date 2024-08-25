d3288746c0883262624e9bad6d8639b48e12632c
Aryann-Yadavv <160234109+Aryann-Yadavv@users.noreply.github.com>
2024-08-26 01:57:32 +0530

    Delete 01_basics/03_conversion&operations.js

--- 01_basics/03_conversion&operations.js
+++
@@ -1,8 +0,0 @@
-let score = 56; // can't tell if score is supposed to be a string or number or object, when it is sent from front end to the back end, for that conversion is used
-
-// you may use typeof function to get it's dataype
-
-console.log(typeof score);
-console.log(typeof (score)); /* here we used () within log(), these are used to represent methods, 
-methods are usually used to access and manipulate the data stored in an object */
-
