507f04f9be2d9840c2677844a1ca5fdaa77aa185
Aryann-Yadavv <160234109+Aryann-Yadavv@users.noreply.github.com>
2024-08-25 21:49:15 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,12 @@
+console.log(2 > 1); gives true
+
+//while comparing make sure the datatype of both items being compared is same
+
+console.log(null > 0); // == gives true as equality check works differently compared to comparison operations, here it treats null as 0, hence it gives null==0 true
+console.log(null <= 0);
+console.log(null == 0);
+
+// === is a strict check, it compares the values strictly, while also strictly checking their data types
+
+console.log("2" == 2);
+console.log("2" === 2); //gives false as "2" is string and 2 is number, can't be compared
