4868016f97c0945426cdee0afda20401984f621d
Aryann-Yadavv <160234109+Aryann-Yadavv@users.noreply.github.com>
2024-09-02 12:18:58 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,53 @@
+// const TinderUser = new Object() - this is a singleton oject
+
+//will give {} as the object is empty
+
+const TinderUser = {} //this is a non-singleton object
+
+/*A singleton has the property that every function from it to any arbitrary set is injective i.e a function that maps distinct elements of its domain to distinct elements of its codomain
+The only non-singleton set with this property is the empty set. The only non-singleton set with this property is the empty set*/
+
+TinderUser.id = "123abc"
+TinderUser.name = "sammy"
+TinderUser.isLoggedIn = false
+
+// console.log(TinderUser);
+
+// const regularUser = {
+//     email: "some@gmail.com"
+//     fullName: {
+//         userFullName: {
+//             firstName: "Aryan"
+//             lastName: "Yadav"
+//             }
+//         } //objects within an object
+//     }
+// }
+
+// console.log(regularUser.fullName.userFullName.firstName);
+
+const obj1 = {a: "1", b: "2"}
+const obj2 = {c: "1", d: "2"}
+
+//const obj3 = {obj1, obj2}
+
+//console.log(obj3); //this will embed those two objects to 1, with 2 objects being obj 1 and obj2, will not make an object of all the keys wihtin obj1 and obj2
+
+//for proper combination we use Object.assign
+
+//const obj3 = Object.assign({}, obj1, obj2) //use {}, it's an optional parameter, 
+//and it'll combine all the keys, where first input is always the target,
+//so {} will act as the target and rest obj1 and obj2 will act as the source
+
+// as Object.assign adds target to source, where object.assign(target, object) is it's psuedocode, where sources are added to objects
+
+
+//best way to combine objects is same as arrays, i.e SPREAD
+
+const obj3 = {...obj1, ...obj2}
+
+console.log(obj3)
+
+// "===" is a boolen equal sign, to check whether values on rhs and lhs are equal or not, it returns true if equal and false if not equal
+
+
